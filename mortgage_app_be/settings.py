948c016747031eec4793cf8a9a2101cf3d740da5
samueltanner <samuel.m.s.tanner@gmail.com>
2023-03-22 15:30:40 -1000

    relocates secrets

--- mortgage_app_be/settings.py
+++ mortgage_app_be/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from ..secrets import SECRET_KEY_CONST
+from app_secrets import SECRET_KEY_STRING
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY_CONST
+SECRET_KEY = SECRET_KEY_STRING
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
