23f03049b859a37f5d1dadbc130a0a5537e9a8bc
samueltanner <samuel.m.s.tanner@gmail.com>
2023-03-23 11:15:23 -1000

    more changes to db

--- mortgage_backend/models.py
+++ mortgage_backend/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class County(models.Model):
-    name = models.CharField(max_length=255)
+    county_name = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     state_abbr = models.CharField(max_length=10)
     median_home_value = models.IntegerField()
+
     def __str__(self):
-        return self.name
+        return self.county_name
+
+# class FHA_Loan_Limit()
