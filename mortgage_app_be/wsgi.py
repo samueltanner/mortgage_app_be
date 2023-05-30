8b51913ee1e5bff7e420a521dfe8a0aa5524c52f
Sam Tanner <samuel.m.s.tanner@gmail.com>
2023-05-30 16:34:47 -0700

    starts getting ready for vercel deploy

--- mortgage_app_be/wsgi.py
+++ mortgage_app_be/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mortgage_app_be.settings')
 
 application = get_wsgi_application()
+
+app = application
