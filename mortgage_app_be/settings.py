0a81779eff30c386f47cbd880bb0bf013a973f3f
Sam Tanner <samuel.m.s.tanner@gmail.com>
2023-05-30 17:14:34 -0700

    undoes vercel actions

--- mortgage_app_be/settings.py
+++ mortgage_app_be/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY_STRING
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
