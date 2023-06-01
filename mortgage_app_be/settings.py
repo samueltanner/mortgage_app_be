4299123434f9dab32b4e8af0d53e304a913014a3
Sam Tanner <samuel.m.s.tanner@gmail.com>
2023-05-31 20:47:02 -0700

    tons of changes hoping to deploy to eb

--- mortgage_app_be/settings.py
+++ mortgage_app_be/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from app_secrets import SECRET_KEY_STRING
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,8 @@ SECRET_KEY = SECRET_KEY_STRING
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-mortgage-env.eba-nvmpnqkp.us-west-2.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
@@ -128,6 +130,8 @@ TIMEOUT = 600
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
